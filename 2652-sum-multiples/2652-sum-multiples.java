ad14660b875e408997a4893e0e79af74542fff21
Suyashmishra018 <suyashm609@gmail.com>
2026-08-01 22:27:01 +0530

    Time: 3 ms (95.62%), Space: 42.6 MB (43.71%) - LeetHub

--- 2652-sum-multiples/2652-sum-multiples.java
+++ 2652-sum-multiples/2652-sum-multiples.java
@@ -2,8 +2,9 @@ class Solution {
     public int sumOfMultiples(int n) {
         int s=0;
         for(int i=1;i<=n;i++){
-             if(i%3==0 || i%5==0 || i%7==0)
+             if(i%3==0 || i%5==0 || i%7==0){
                 s+=i;
+             }
         }
         return s;
     }
