5368a541db961464be0da97eef6829d55f4a8ed4
Suyashmishra018 <suyashm609@gmail.com>
2026-08-23 23:17:05 +0530

    Time: 30 ms (77.43%), Space: 44.2 MB (91.55%) - LeetHub

---
+++ 0279-perfect-squares/0279-perfect-squares.java
@@ -0,0 +1,11 @@
+class Solution {
+    public int numSquares(int n) {
+        int[] ans=new int[n+1];
+        for(int i=0;i<=n;i++){
+            ans[i]=i;
+            for(int j=1;j*j<=i;j++)
+            ans[i]=Math.min(ans[i],ans[i-j*j]+1);
+        }
+        return ans[n];
+    }
+}
