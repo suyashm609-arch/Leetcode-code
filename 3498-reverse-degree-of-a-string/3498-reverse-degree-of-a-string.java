b979b99929540caaa8fe0209c58723f3927de3ad
Suyashmishra018 <suyashm609@gmail.com>
2026-09-20 12:04:15 +0530

    Time: 1 ms (100%), Space: 44.2 MB (34.29%) - LeetHub

--- 3498-reverse-degree-of-a-string/3498-reverse-degree-of-a-string.java
+++ 3498-reverse-degree-of-a-string/3498-reverse-degree-of-a-string.java
@@ -2,8 +2,7 @@ class Solution {
     public int reverseDegree(String s) {
         int a=0;
         for(int i=0;i<s.length();i++){
-            int r=26-(s.charAt(i)-'a');
-            a+=(i+1)*r;
+           a+=('z'-s.charAt(i)+1)*(i+1);
         }
         return a;
     }
